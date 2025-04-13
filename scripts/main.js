6e0cffe71c4222476663fd0c459c7216f5fb31fd
LisandraFerraz <lisandraferraz.s@gmail.com>
2025-04-13 00:41:11 -0300

    [FIX] fixing adding videos to days

--- scripts/main.js
+++ scripts/main.js
@@ -24,14 +24,170 @@ var collapseBtn = document.querySelector("#collapser");
 
 let keyword = "";
 const YT_BASE_URL = "https://www.googleapis.com/youtube/v3";
-const maxResults = 20;
+const maxResults = 50;
 
 let isLoading = false;
 let days = [];
 let videosDetails = [];
 
+// let videosDetails = [
+//   {
+//     id: "QqkLnNClpAQ",
+//     publishedAt: "2025-04-03T09:00:35Z",
+//     title: "KAI 카이 &#39;Adult Swim&#39; MV",
+//     thumbnail: "https://i.ytimg.com/vi/QqkLnNClpAQ/hqdefault.jpg",
+//     channelTitle: "SMTOWN",
+//     videoLength: "02:35",
+//   },
+//   {
+//     id: "_I4CoRahpSo",
+//     publishedAt: "2025-04-03T12:19:34Z",
+//     title: "&#39;단독 공개&#39; KAI (카이) - Adu...",
+//     thumbnail: "https://i.ytimg.com/vi/_I4CoRahpSo/hqdefault.jpg",
+//     channelTitle: "Mnet K-POP",
+//     videoLength: "02:48",
+//   },
+//   {
+//     id: "kuOmrRlJDp0",
+//     publishedAt: "2025-04-05T07:20:12Z",
+//     title: "🙌👏🙌👏 #KAI #카이 #EXO #엑소 #we...",
+//     thumbnail: "https://i.ytimg.com/vi/kuOmrRlJDp0/hqdefault.jpg",
+//     channelTitle: "EXO",
+//     videoLength: "00:20",
+//   },
+//   {
+//     id: "oDvpdtxYMPE",
+//     publishedAt: "2023-03-29T09:30:12Z",
+//     title: "EXO KAI&#39;s amazing spin per...",
+//     thumbnail: "https://i.ytimg.com/vi/oDvpdtxYMPE/hqdefault.jpg",
+//     channelTitle: "K-contents Voyage",
+//     videoLength: "00:15",
+//   },
+//   {
+//     id: "zWd_AP_58fI",
+//     publishedAt: "2021-12-05T12:14:29Z",
+//     title: "KAI(카이) - Peaches @인기가요 inkiga...",
+//     thumbnail: "https://i.ytimg.com/vi/zWd_AP_58fI/hqdefault.jpg",
+//     channelTitle: "SBSKPOP X INKIGAYO",
+//     videoLength: "03:48",
+//   },
+//   {
+//     id: "6BhCTJXMdy8",
+//     publishedAt: "2023-03-18T12:30:28Z",
+//     title: "Dancing king &amp; queen Rover...",
+//     thumbnail: "https://i.ytimg.com/vi/6BhCTJXMdy8/hqdefault.jpg",
+//     channelTitle: "EXO",
+//     videoLength: "00:31",
+//   },
+//   {
+//     id: "yb4uNVbT4KY",
+//     publishedAt: "2013-09-28T19:49:35Z",
+//     title: "[댄싱9/Dancing9] Girls&#39; Gene...",
+//     thumbnail: "https://i.ytimg.com/vi/yb4uNVbT4KY/hqdefault.jpg",
+//     channelTitle: "Mnet K-POP",
+//     videoLength: "02:03",
+//   },
+//   {
+//     id: "kUr8X5iZUGU",
+//     publishedAt: "2021-12-04T14:00:16Z",
+//     title: "[BE ORIGINAL] KAI(카이) &#39;Pea...",
+//     thumbnail: "https://i.ytimg.com/vi/kUr8X5iZUGU/hqdefault.jpg",
+//     channelTitle: "STUDIO CHOOM [스튜디오 춤]",
+//     videoLength: "03:27",
+//   },
+//   {
+//     id: "QC7JTQae530",
+//     publishedAt: "2025-04-12T13:00:44Z",
+//     title: "Justin Bieber VS Kai (EXO) Tra...",
+//     thumbnail: "https://i.ytimg.com/vi/QC7JTQae530/hqdefault.jpg",
+//     channelTitle: "Celebrity Evolutions",
+//     videoLength: "08:07",
+//   },
+//   {
+//     id: "X6zB0hPa154",
+//     publishedAt: "2023-01-01T16:32:09Z",
+//     title: "EXO 엑소 &#39;LOVE SHOT&#39; Sta...",
+//     thumbnail: "https://i.ytimg.com/vi/X6zB0hPa154/hqdefault.jpg",
+//     channelTitle: "다다익첸",
+//     videoLength: "03:24",
+//   },
+//   {
+//     id: "Qgqk96TJZyI",
+//     publishedAt: "2023-03-15T14:00:14Z",
+//     title: "[BE ORIGINAL] KAI(카이) &#39;Rov...",
+//     thumbnail: "https://i.ytimg.com/vi/Qgqk96TJZyI/hqdefault.jpg",
+//     channelTitle: "STUDIO CHOOM [스튜디오 춤]",
+//     videoLength: "03:04",
+//   },
+//   {
+//     id: "s_V_ewm__P0",
+//     publishedAt: "2022-06-05T08:10:51Z",
+//     title: "damnnn!🔥👁️👄👁️🤰 #kai #exo ...",
+//     thumbnail: "https://i.ytimg.com/vi/s_V_ewm__P0/hqdefault.jpg",
+//     channelTitle: "𝙼𝚒𝚗'𝚜 𝙼𝚊𝚗𝚍𝚎𝚛𝚒𝚗",
+//     videoLength: "00:15",
+//   },
+//   {
+//     id: "Nn-wZ1MHxwQ",
+//     publishedAt: "2019-06-10T10:51:48Z",
+//     title: "♨핫클립♨[HD][카이 리액션] 스웩 넘치는 카이(EX...",
+//     thumbnail: "https://i.ytimg.com/vi/Nn-wZ1MHxwQ/hqdefault.jpg",
+//     channelTitle: "JTBC Voyage",
+//     videoLength: "13:07",
+//   },
+//   {
+//     id: "zlTIextYnyQ",
+//     publishedAt: "2023-03-13T08:59:10Z",
+//     title: "KAI 카이 &#39;Rover&#39; MV",
+//     thumbnail: "https://i.ytimg.com/vi/zlTIextYnyQ/hqdefault.jpg",
+//     channelTitle: "SMTOWN",
+//     videoLength: "03:03",
+//   },
+//   {
+//     id: "lOX73kBRyWs",
+//     publishedAt: "2022-12-26T09:00:20Z",
+//     title: "KAI, SEULGI, JENO, KARINA &#39...",
+//     thumbnail: "https://i.ytimg.com/vi/lOX73kBRyWs/hqdefault.jpg",
+//     channelTitle: "SMTOWN",
+//     videoLength: "03:35",
+//   },
+//   {
+//     id: "8fcOLBawlkU",
+//     publishedAt: "2022-11-15T01:26:38Z",
+//     title: "Mmmh - KAI カイ(EXO エクソ) [STAGE ...",
+//     thumbnail: "https://i.ytimg.com/vi/8fcOLBawlkU/hqdefault.jpg",
+//     channelTitle: "KBS WORLD TV",
+//     videoLength: "03:18",
+//   },
+//   {
+//     id: "lz2opWX2dw0",
+//     publishedAt: "2025-04-08T07:22:39Z",
+//     title: "How hard is KAI 카이 - ‘Adult Sw...",
+//     thumbnail: "https://i.ytimg.com/vi/lz2opWX2dw0/hqdefault.jpg",
+//     channelTitle: "Kathleen Carm",
+//     videoLength: "00:28",
+//   },
+//   {
+//     id: "wbMrPXF8P3M",
+//     publishedAt: "2023-04-18T04:43:12Z",
+//     title: "These idols reaction really wa...",
+//     thumbnail: "https://i.ytimg.com/vi/wbMrPXF8P3M/hqdefault.jpg",
+//     channelTitle: "Kpoplands567",
+//     videoLength: "00:20",
+//   },
+//   {
+//     id: "vN_TuxkMjIE",
+//     publishedAt: "2020-12-04T09:22:17Z",
+//     title: "KAI - Mmmh(음) (Music Bank) |...",
+//     thumbnail: "https://i.ytimg.com/vi/vN_TuxkMjIE/hqdefault.jpg",
+//     channelTitle: "KBS WORLD TV",
+//     videoLength: "03:24",
+//   },
+// ];
+
 // Recebe o resultado dos fetches videos e detalhes
 // cria um objeto com os todos os atributos necessários de ambos resultados
+
 async function organizeVideoDetails() {
   const videosList = await fetchSearchTerm(); //detalhes de videosList
   const videosFiltered = await fetchVideosDetails(); //detalhes de videosList
@@ -61,15 +217,15 @@ async function organizeVideoDetails() {
 
 // transforma minutos/horas em segundos ou retorna somente segundos
 function calcWatchTime(videoLength = "") {
-  const timeSplit = videoLength.split(":").map(Number);
+  const parts = videoLength.split(":").map(Number);
 
-  if (timeSplit.length === 3) {
+  if (parts.length === 3) {
     // hh:mm:ss
-    const [h, m, s] = timeSplit;
+    const [h, m, s] = parts;
     return h * 3600 + m * 60 + s;
-  } else if (timeSplit.length === 2) {
+  } else if (parts.length === 2) {
     // mm:ss
-    const [m, s] = timeSplit;
+    const [m, s] = parts;
     return m * 60 + s;
   } else {
     return 0;
@@ -228,21 +384,32 @@ function addVideosToDays() {
   for (let i = 0; i < daysWithVideos.length; i++) {
     let availTime = daysWithVideos[i].availMinutes * 60;
 
-    const videoTime = calcWatchTime(videosDetails[videoIndex]?.videoLength);
+    while (videoIndex < videosDetails.length) {
+      let videoFits = false;
 
-    if (videoTime < availTime) {
-      videoIndex++;
+      for (let v = videoIndex; v < videosDetails.length; v++) {
+        const video = videosDetails[v];
+        const videoTime = calcWatchTime(video.videoLength);
 
-      daysWithVideos[i].videosList.push(videosDetails[videoIndex]);
-      daysWithVideos[i].vidQty = daysWithVideos[i].videosList?.length;
+        if (videoTime <= availTime) {
+          daysWithVideos[i].videosList.push(video);
+          daysWithVideos[i].vidQty = daysWithVideos[i].videosList.length;
+          availTime -= videoTime;
 
-      availTime -= videoTime;
+          videosDetails.splice(v, 1);
+          videoFits = true;
+          break;
+        }
+      }
+
+      if (!videoFits) {
+        break;
+      }
     }
   }
 
+  days = daysWithVideos;
   renderDays();
-  console.log("Resultado atribuição | ", daysWithVideos);
-  console.log("videosDetails | ", videosDetails);
 }
 
 // Collapser do calendário
@@ -269,6 +436,9 @@ searchBar.addEventListener("change", (event) => {
 
 // Pesquisar palavra-chave
 submitBtn.addEventListener("click", () => {
+  // renderVideosThumb(videosGroupDiv, videosDetails);
+  // addVideosToDays();
+
   if (keyword !== "") {
     organizeVideoDetails();
   } else {
